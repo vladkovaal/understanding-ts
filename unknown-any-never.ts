f6412490554b1f24f5674fe26da0ea55ecd386d4
Vladyslav <kovalchuk.vlad01@gmail.com>
2022-07-14 03:35:51 +0300

    Added unknown-any-never.ts
    understanding unknown, any and never types in ts

---
+++ unknown-any-never.ts
@@ -0,0 +1,17 @@
+let userInput: unknown;
+let anyUserInput: any;
+let userName: string;
+
+userInput = 'input';
+
+if (typeof userInput === 'string') {
+	userName = userInput;
+}
+
+userName = anyUserInput;
+
+function genErr(msg: string, code: number) {
+	throw { message: msg, errorCode: code };
+}
+
+genErr('Not found ', 404);
